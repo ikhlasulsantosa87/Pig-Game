56d1760ab4cc0ad7f3dda061155a1ab84cfb4a95
Ikhlasul Amal Santosa <ikhlasulsantosa87@gmail.com>
2023-11-23 15:02:41 +0700

    Updated the names of the player in the game

--- dice.py
+++ dice.py
@@ -1,8 +1,14 @@
-import random
+import random # Python built-in modules to make random numbers
 
 def dice():
+    
+    """
+        Definitions: A function that acts as a dice. Using a random module and random.randint() method from Python to simulate the roll of a dice. Returns a random number from 1 - 6.
+    """
+
     min_number = 1
     max_number = 6
+
     dice = random.randint(min_number, max_number)
 
     return dice
--- pig_game.py
+++ pig_game.py
@@ -1,5 +1,6 @@
-from dice import dice
+from dice import dice # Import the dice() function that we created
 
+# A loop that asking for how many players will play the game. If it 2 or less than equal to 4, the game proceeds and the loop breaks.
 while True:
     try:
         player_number = int(input("How many players that want to play the game of Pig: "))
@@ -11,23 +12,38 @@ while True:
     except ValueError:
         print("Invalid Input! Try Again!")
 
-winning_score = 20
-scoreboard = [0 for _ in range(player_number)]
+# Asking for the registered player their name and welcomes them
+player_names = []
 
+for _ in range(player_number):
+    username = input("Enter your name: ")
+    player_names.append(username)
+
+print(f"Welcome {', '.join(player_names)}\n")
+
+winning_score = 20 # The winning score for the game
+scoreboard = [0 for _ in range(len(player_names))] # To store each player score and keep tracks it throughout the game
+
+# If the winning score is not reach by either player, the game still on
 while max(scoreboard) < winning_score:
-    for player_list in range(player_number):
-        print("It's player {} turn. Let's Go!".format(player_list + 1))
+
+    # Simulating of each player turn in the game
+    for player_list in range(len(player_names)):
+        print("It's {} turn. Let's Go!".format(player_names[player_list]))
         print("Your current score is {}".format(scoreboard[player_list]))
         current_score = 0
         
+        # Ask the user for confirmation whether they want to continue with their turn or stop.
         while True:
             player_prompt = input("Do you want to keep rolling or not: Type 'y' to continue - ")
             if player_prompt != "y":
                 print("You ended your turn.\n")
                 break
 
+            # Show the number of the dice that player of that turn rolled.    
             dice_number = dice()
 
+            # Check if the number showed by the dice. If the player gets 1, all of their score in that turn is reset to 0 and if it is not it adds to their scoreboard.
             if dice_number == 1:
                 print("Oopsss! You just rolled {}. Your turn is done".format(dice_number))
                 current_score = 0
@@ -37,11 +53,13 @@ while max(scoreboard) < winning_score:
                 current_score += dice_number
 
             print("Your total score in this turn is {}".format(current_score))
-
+        # Add the player score for that turn into the scoreboard to keep track.
         scoreboard[player_list] += current_score
-        print("Player {} total score in this game is {}\n".format(player_list + 1, scoreboard[player_list]))
+        print("{} total score in this game is {}\n".format(player_names[player_list], scoreboard[player_list]))
 
+# Determining who is the winner of the game
 game_result = max(scoreboard)
 winning_player = scoreboard.index(game_result)
 
-print("Congratulations player {} for winning the game of Pig. Your total score is {}".format(winning_player + 1, game_result))
+# Display the winner
+print("Congratulations {} for winning the game of Pig. Your total score is {}".format(player_names[winning_player], game_result))
